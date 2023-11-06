c88cb7654e55d319ef9c10fec1e375cc3671a78e
helgacozzo <helgacozzo@gmail.com>
2023-11-06 16:13:32 +0100

    feat/ esercizio 2

--- main.js
+++ main.js
@@ -1,9 +1,37 @@
 // Esercizio 1: Crea una arrow function che accetta due numeri come parametri 
 // e restituisce la loro sottrazione.
 
-const sottrazione = (num1, num2) => {
-    return num1 - num2;
-}
+// const sottrazione = (num1, num2) => {
+//     return num1 - num2;
+// }
 
-const sottrazione1 = sottrazione(2, 2);
-console.log (sottrazione1);
+// const sottrazione1 = sottrazione(2, 2);
+// console.log (sottrazione1);
+
+
+
+// Esercizio 2: Crea un oggetto chiamato 'studente'. 
+// Dovrebbe avere proprietà per 'nome' e 'età'. 
+// Dovrebbe anche avere un metodo che usa la parola chiave 'this' per stampare il nome e l'età 
+// dello studente e un metodo per incrementare l'età.
+
+// const studente = {
+//     nome: 'gigi',
+//     cognome: 'rossi',
+//     eta: 17,
+//     etaAumenta: function(){
+//         this.eta ++;
+//     },
+//     informazioni: function(){
+//         console.log(this.nome);
+//         console.log(this.eta);
+//     }
+// };
+
+// studente.etaAumenta();
+// studente.informazioni()
+
+
+
+// Esercizio 3: Crea una semplice pagina HTML con un elemento div. 
+// Usa JavaScript per cambiare l'innerHTML e l'innerText del div.
