6745719ca6ce66238166a0f12d0333212f68be00
helgacozzo <helgacozzo@gmail.com>
2023-11-06 15:10:56 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+// Esercizio 1: Crea una arrow function che accetta due numeri come parametri 
+// e restituisce la loro sottrazione.
+
+const sottrazione = (num1, num2) => {
+    return num1 - num2;
+}
+
+const sottrazione1 = sottrazione(2, 2);
+console.log (sottrazione1);
