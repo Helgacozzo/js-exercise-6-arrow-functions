56a42d3ee98d6ea5e085fba6d6706338c916ce10
helgacozzo <helgacozzo@gmail.com>
2023-11-06 17:47:01 +0100

    feat/esercizio 4

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 // Esercizio 1: Crea una arrow function che accetta due numeri come parametri 
 // e restituisce la loro sottrazione.
 
-// const sottrazione = (num1, num2) => {
-//     return num1 - num2;
-// }
+// const sottrazione = (num1, num2) => num1 - num2;
 
 // const sottrazione1 = sottrazione(2, 2);
 // console.log (sottrazione1);
@@ -35,3 +33,44 @@
 
 // Esercizio 3: Crea una semplice pagina HTML con un elemento div. 
 // Usa JavaScript per cambiare l'innerHTML e l'innerText del div.
+
+// window.addEventListener('load', () =>{
+//     const body = document.body;
+//     body.innerText = ' Hello World!';
+//     body.innerHTML += '<h5>Sottotitolo</h5>';
+// });
+
+
+// Esercizio 4: Crea una lista non ordinata con tre elementi di lista.
+// Usa JavaScript per aggiungere un nuovo elemento di lista alla fine della lista usando appendChild.
+// Poi usa insertBefore per aggiungere un elemento di lista all'inizio della lista.
+
+const listaPietre = ['Quarzo rosa', 'Ossidiana', 'Ametista']
+
+window.addEventListener('load', () =>{
+    const body = document.body;
+
+    const ul = document.createElement('ul');
+    for(let i=0; i<listaPietre.length; i++){
+        const li = document.createElement('li');
+       li.innerText = listaPietre[i];
+       ul.appendChild(li);
+    }
+
+    body.appendChild(ul);
+
+});
+
+
+// Esercizio 5: Crea un elemento div con class 'blu'.
+// Usa JavaScript per controllare al click se il div ha la classe 'blu' usando classList.includes().
+// Poi usa classList.add() per aggiungere una classe 'rosso' e classList.remove() per rimuovere la classe 'blu'.
+
+
+// Esercizio 6: Crea un pulsante in HTML. 
+// Usa JavaScript per impostare un attributo 'disabled' sul pulsante usando setAttribute().
+
+
+// Esercizio 7: Crea un elemento div. Aggiungi un evento onmouseover che cambia il colore del div. 
+// Aggiungi un evento onmouseout che cambia il colore del div indietro.
+// Aggiungi un evento click che stampa un messaggio in console.
